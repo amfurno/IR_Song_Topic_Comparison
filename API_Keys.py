1a20a473d3ad030bd45f90884fcaff565f81db04
Anthony <hockeyamf@gmail.com>
2021-04-08 13:02:02 -0500

    remvoe api keys

--- API_Keys.py
+++
@@ -1,4 +0,0 @@
-spotify_client_id = '272388e7b578442ebee652328cb199ec'
-spotify_secret = 'f92e7359a1eb412aa9065f2296daf202'
-spotify_redirect_uri = 'https://google.com'
-genius_access_token = 'vq2pMXESMHPg9Yp85YBksfhK1oLneT0M7jdob0Xx9Waa_ibVBObGvycjnMU6-7Z7'
