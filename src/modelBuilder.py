c2cd902a34f536a62b35e4c448ccc6c1a3ea44d6
Anthony <hockeyamf@gmail.com>
2021-04-16 10:30:26 -0500

    save model to file

--- src/modelBuilder.py
+++ src/modelBuilder.py
@@ -1,6 +1,7 @@
 from gensim.corpora import Dictionary
-from gensim.models import LdaModel
+from gensim.models import LdaModel, ldamodel
 from gensim.models.coherencemodel import CoherenceModel
+from gensim.test.utils import datapath
 from nltk.corpus import stopwords
 from pprint import pprint
 import logging
@@ -9,7 +10,7 @@ import csv
 if __name__ == '__main__':
 
     logging.basicConfig(
-        filename='model.log', format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+        filename='model.log', format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
     docs = []
     with open('outputs/corpus.txt', mode='r') as lyrics:
@@ -17,47 +18,41 @@ if __name__ == '__main__':
             docs.append(line.split())
 
     dictionary = Dictionary(docs)
-    dictionary.filter_extremes(no_below=10, no_above=0.5)
+    dictionary.filter_extremes(no_below=20, no_above=0.5)
     corpus = [dictionary.doc2bow(doc) for doc in docs]
 
     print('Number of unique tokens: %d' % len(dictionary))
     print('Number of documents: %d' % len(corpus))
 
     # Set training parameters.
-    num_topics = 10
+    num_topics = 50
     chunksize = 2000
-    passes = 50
+    passes = 10
     iterations = 400
     eval_every = None  # Don't evaluate model perplexity, takes too much time.
 
 # Make a index to word dictionary.
     temp = dictionary[0]  # This is only to "load" the dictionary.
     id2word = dictionary.id2token
-    with open("outputs/topicCoherence.csv", mode='a', newline='') as coherenceCSV:
+    model = LdaModel(
+        corpus=corpus,
+        id2word=id2word,
+        chunksize=chunksize,
+        alpha='auto',
+        eta='auto',
+        iterations=iterations,
+        num_topics=num_topics,
+        passes=passes,
+        eval_every=eval_every
+    )
+    # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    coherenceModel = CoherenceModel(
+        model=model, texts=docs, dictionary=dictionary, coherence='c_v')
 
-        coherenceWriter = csv.writer(
-            coherenceCSV, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        coherenceWriter.writerow(
-            ['Number of topics', 'coherence model value', 'avg topic coherence'])
-
-        for i in range(200, 401, 20):
-            model = LdaModel(
-                corpus=corpus,
-                id2word=id2word,
-                chunksize=chunksize,
-                alpha='auto',
-                eta='auto',
-                iterations=iterations,
-                num_topics=i,
-                passes=passes,
-                eval_every=eval_every
-            )
-            # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
-            coherenceModel = CoherenceModel(
-                model=model, texts=docs, dictionary=dictionary, coherence='c_v')
-            top_topics = model.top_topics(corpus)  # , num_words=20)
-            avg_topic_coherence = sum([t[1] for t in top_topics]) / i
-            coherenceWriter.writerow(
-                [i, coherenceModel.get_coherence(), avg_topic_coherence])
-            print('Average topic coherence for %d topics: %.4f.' %
-                  (i, avg_topic_coherence))
+    top_topics = model.top_topics(corpus)  # , num_words=20)
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    print('Average UMass topic coherence for %d topics: %.4f.' %
+          (num_topics, avg_topic_coherence))
+    print('Average C_V topic coherence for %d topics: %.4f.' %
+          (num_topics, coherenceModel.get_coherence()))
+    model.save("outputs/model/model")
