d8468466e6eef797aa1498e64db672f090f22807
Anthony <hockeyamf@gmail.com>
2021-04-16 14:30:22 -0500

    can no get text from boxes

--- src/gui.py
+++ src/gui.py
@@ -1,47 +1,56 @@
 import tkinter
-from tkinter import *
 import tkinter.font as font
+from tkinter import *
+
+import songComparison
 
 # creates window
-r = tkinter.Tk()
-
-# sets window size
-r.geometry('650x300')
-
-# sets background color of window
-r.configure(bg='pink')
-
-#r.place_slaves(rely=0.5, anchor=N)
-
-# set font
-myFont = font.Font(family='Courier')
-
-# sets title
-r.title('Song Lyric Comparison')
-
-# sets labels for boxes
-Label(r, text='Artist 1 Name', font=myFont, bg='pink').grid(row=0, column=0, )
-Label(r, text='Artist 2 Name', font=myFont, bg='pink').grid(row=0, column=2)
-Label(r, text='Song 1 Name', font=myFont, bg='pink').grid(row=1, column=0)
-Label(r, text='Song 2 Name', font=myFont, bg='pink').grid(row=1, column=2)
-
-# makes input boxes
-artist1Name = Entry(r, textvariable=artist1N)
-artist2Name = Entry(r)
-song1Name = Entry(r)
-song2Name = Entry(r)
 
 
-# puts in right place
-artist1Name.grid(row=0, column=1)
-artist2Name.grid(row=0, column=3)
-song1Name.grid(row=1, column=1)
-song2Name.grid(row=1, column=3)
+class comparisonApp:
+    def __init__(self):
+        app = Tk()
 
-# button
-clickGo = tkinter.Button(r, text='Compare My Songs!', bg='pink',
-                         width=20, font=myFont, highlightcolor='blue', command=r.destroy)
-clickGo.place(x=300, y=100, anchor=CENTER)
+        self.myfont = font.Font(family='Courier')
+        app.geometry('650x300')
+        app.configure(bg='pink')
+        app.title('Song Lyric Comparison')
+
+        self.artist1 = StringVar()
+        self.artist2 = StringVar()
+        self.song1 = StringVar()
+        self.song2 = StringVar()
+
+        Label(app, text='Artist 1 Name', font=self.myfont,
+              bg='pink').grid(row=0, column=0, )
+        Label(app, text='Artist 2 Name', font=self.myfont,
+              bg='pink').grid(row=0, column=2)
+        Label(app, text='Song 1 Name', font=self.myfont,
+              bg='pink').grid(row=1, column=0)
+        Label(app, text='Song 2 Name', font=self.myfont,
+              bg='pink').grid(row=1, column=2)
+
+        artist1Box = Entry(app, textvariable=self.artist1,).grid(
+            row=0, column=1)
+        artist2Box = Entry(app, textvariable=self.artist2).grid(
+            row=0, column=3)
+        song1Box = Entry(app, textvariable=self.song1).grid(
+            row=1, column=1)
+        song2Box = Entry(app, textvariable=self.song2).grid(
+            row=1, column=3)
+
+        Button(app, text='Compare My Songs!', bg='pink',
+               width=20, font=self.myfont, highlightcolor='blue',
+               command=self.compareSongs).place(x=300, y=100, anchor=CENTER)
+
+        app.mainloop()
+
+    def compareSongs(self):
+        print(self.artist1.get())
+
 
 # runs window
-r.mainloop()
+if __name__ == '__main__':
+
+    comparisonApp()
+    # clickGo.place(x=300, y=100, anchor=CENTER)
